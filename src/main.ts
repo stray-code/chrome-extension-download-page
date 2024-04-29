390b474cf66f09745c94adbb511b0a2cb1733bea
stray-code <abc1007020@gmail.com>
2024-04-29 21:37:17 +0900

    husky追加と設定

--- src/main.ts
+++ src/main.ts
@@ -21,4 +21,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);

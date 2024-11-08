8e30553086af01ead2c7bacaa115319ffa7551c6
stray-code <abc1007020@gmail.com>
2024-11-09 07:47:18 +0900

    バージョンを1.0.1に更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "Webサイトダウンロード",
   description: "Webサイト（ページ）をダウンロードします。",
-  version: "1.0.0",
+  version: "1.0.1",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",

7315f80f8a40fa66d835b268dab84a6db70761a3
stray-code <abc1007020@gmail.com>
2025-10-05 12:14:39 +0900

    format対象を.に

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+import { defineConfig } from 'vite';
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "Webサイトダウンロード",
-  description: "Webサイト（ページ）をダウンロードします。",
-  version: "1.0.1",
+  name: 'Webサイトダウンロード',
+  description: 'Webサイト（ページ）をダウンロードします。',
+  version: '1.0.1',
   icons: {
-    16: "img/icon16.png",
-    48: "img/icon48.png",
-    128: "img/icon128.png",
+    16: 'img/icon16.png',
+    48: 'img/icon48.png',
+    128: 'img/icon128.png',
   },
   action: {
-    default_popup: "src/popup/index.html",
+    default_popup: 'src/popup/index.html',
   },
-  permissions: ["tabs", "pageCapture"],
+  permissions: ['tabs', 'pageCapture'],
 });
 
 export default defineConfig({
